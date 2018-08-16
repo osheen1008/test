be0b3e3313fff5a6c5cdae0e2cfdd3bcbd68fa12
Dari <590451@cognizant.com>
2018-08-16 11:27:35 +0530

    terraform test

---
+++ test.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "test" {
+	name	= "590451-test"
+	ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "590451"
+  }
+	}
+	
+output "sg_id" {
+	value = "${aws_security_group.test.id}"
+	}
