cecd1c29b6038dd40d46190b805b935d05a5ed30
Osheen Dari <osheen1008@gmail.com>
2018-08-16 16:09:51 +0530

    updated script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,1 @@
-set type = %1
-echo type
-echo "Deploying infrastructure..."
-if %type% == "destroy"
-(
-terraform %type% --force
-)
-elif %type% -eq "apply" 
-(
-terraform %type% -auto-approve
-)
-else
-(
-terraform %type%
-)
-terraform output
+terraform plan
