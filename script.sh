377227a3164d2084bccfe7f836d5aec445e6fa57
Osheen Dari <osheen1008@gmail.com>
2018-08-25 23:54:12 +0530

    script updated

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 echo $1
 terraform init
-if [$1 = 'destroy']
+if [ $1 -eq "destroy" ]
 then
 echo "About to Destroy"
 terraform $1 -force
-elif [$1 = 'apply']
+elif [ $1 = "apply" ]
 then
 echo "About to apply"
 terraform $1 -auto-approve
